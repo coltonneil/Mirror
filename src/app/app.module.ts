5c6b143d33815134fd5830a09b3e3031a633144a
Colton Williams <coltonneilwilliams@gmail.com>
2018-10-02 23:43:09 -0700

    Creates scaffolding for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuIconComponent } from './menu-icon/menu-icon.component';
+import { LogoComponent } from './logo/logo.component';
+import { SearchIconComponent } from './search-icon/search-icon.component';
+import { AccountIconComponent } from './account-icon/account-icon.component';
+import { CartIconComponent } from './cart-icon/cart-icon.component';
+import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
+import { HeaderIconsComponent } from './header-icons/header-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuIconComponent,
+    LogoComponent,
+    SearchIconComponent,
+    AccountIconComponent,
+    CartIconComponent,
+    MobileHeaderComponent,
+    HeaderIconsComponent
   ],
   imports: [
     BrowserModule
